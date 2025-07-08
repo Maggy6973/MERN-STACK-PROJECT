eecc4c19df9e53f71439c05b228b06c0d50fb2fd
Maggy6973 <margaretsisia88@gmail.com>
2025-07-08 12:22:37 -0700

    changes

--- server/userSeed.js
+++ server/userSeed.js
@@ -1,6 +1,7 @@
 import User from './models/User.js'
 import bcrypt from 'bcrypt'
 import connectToDatabase from './db/db.js'
+import 'dotenv/config'; // Ensure you have dotenv installed and configured
 
 const userRegister = async () =>{
     connectToDatabase()
